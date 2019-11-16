3715e59136b8c045f1626539a7d6cdee00d31d48
Ale Kiut <ale_kiut47@hotmail.com>
2019-11-15 19:08:36 -0600

    cambios

--- ejercicios.js
+++ ejercicios.js
@@ -1,25 +1,8 @@
-let login = document.getElementById("login")
-let usuario = document.getElementById("nombre");
-let email = document.getElementById("email");
+let buscador = document.getElementById('buscador')
 
-let enviar = document.getElementById("enviar");
-let valor1 = document.getElementById("valor1");
-let valor2 = document.getElementById("valor2");
+const mostrarbuscador = () =>{
 
-let formcheck =document.getElementById()
-
-const login = () => {
-    let valorUsuario = usuario.value;
-    let valorEmail = email.value;
-
-    valor1.innerHTML = usuario.value;
-    valor2.innerHTML = email.value;
-
-    let usuarioCorrecto= "Juan";
-    let emailCorrecto = "a@.com";
-
-    if(valorusuario === nombreCorrecto && valorEmail === emailCorrecto){
-        validar.innerHTML = "Bienvenido"
-     } 
+    window.location.href="./cs.html"
 }
-enviar.addEventListener("click", login)
+
+buscador.addEventListener('click', mostrarbuscador)
